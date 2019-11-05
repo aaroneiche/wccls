62413cb621b9b41a252e44d2c82571f25571ff29
Aaron Eiche <aaron@aaroneiche.com>
2019-11-05 07:58:37 -0800

    Initial work

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="header">
+      <input name="search" type="text" default="Robots" />
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+#header {
+
+}
+
+
+input {
+  font-size: 24pt;
+}
+</style>
